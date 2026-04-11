b32025869054867fa4766050b5bc9e5e1a5c1125
rei-nld <reinbld@gmail.com>
2026-04-11 16:29:12 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";
+  };
+  outputs = { self, nixpkgs, proxmox-nixos, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        modules = [ 
+          ./configuration.nix
+          proxmox-nixos.nixosModules.proxmox-ve
+          
+          ({ pkgs, lib, ... }: {
+            services.proxmox-ve = {
+              enable = true;
+              ipAddress = "192.168.0.250";
+            };
+            nixpkgs.overlays = [
+              proxmox-nixos.overlays.${system}
+            ];
+          })
+        ];
+      };
+    };
+  };
+}
